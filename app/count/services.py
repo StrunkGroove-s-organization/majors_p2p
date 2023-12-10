252a15f0fc5d3f7e8e5a6e1be3e6e6235a1456e9
StrunkGroove <jmarkj@mail.ru>
2023-12-11 01:20:46 +0300

    rework parsers all work

--- app/count/services.py
+++ app/count/services.py
@@ -123,6 +123,28 @@ class BaseCount(BaseP2P):
         }
 
 
+    def exchange_query(self, ex: str, site: str, pay: str, offset=1) -> str:
+        sort_direction = "ASC" if site == "BUY" else "DESC"
+        query = f"""
+            (
+                SELECT
+                    name, order_q, order_p, price, lim_min, lim_max, fiat,
+                    token, buy_sell, exchange, adv_no,  '{pay}' as best_payment,
+                    available_rub
+                FROM public.{AsIs(ex)}
+                WHERE token = %s
+                AND buy_sell = %s
+                AND '{pay}' IN (
+                    SELECT value::text FROM jsonb_array_elements_text(payments)
+                )
+                ORDER BY price {sort_direction}
+                LIMIT 1
+                OFFSET {offset}
+            )
+        """
+        return query
+    
+
     def create_exchange_query(self) -> tuple:
         union_queries = []
         params = []
@@ -144,28 +166,8 @@ class BaseCount(BaseP2P):
         return full_query, params
 
 
-    def exchange_query(self, ex: str, site: str, pay: str) -> str:
-        sort_direction = "ASC" if site == "BUY" else "DESC"
-        query = f"""
-            (
-                SELECT
-                    name, order_q, order_p, price, lim_min, lim_max, fiat,
-                    token, buy_sell, exchange, adv_no,  '{pay}' as best_payment,
-                    available_rub
-                FROM public.{AsIs(ex)}
-                WHERE token = %s
-                AND buy_sell = %s
-                AND '{pay}' IN (
-                    SELECT value::text FROM jsonb_array_elements_text(payments)
-                )
-                ORDER BY price {sort_direction}
-                LIMIT 2
-            )
-        """
-        return query
-
-
-    def execute_exchange_query(self, query: str, params: list) -> tuple:
+    def execute_exchange_query(self) -> tuple:
+        query, params = self.create_exchange_query()
         with connection.cursor() as cursor:
             cursor.execute(query, params)
             result = cursor.fetchall()
@@ -173,23 +175,27 @@ class BaseCount(BaseP2P):
 
 
     def split_and_sort_ads(self, ads: list) -> tuple:
-        buy_ads = []
-        sell_ads = []
+        buy_ads = {}
+        sell_ads = {}
 
         for ad in ads:
             if ad['buy_sell'] == 'BUY':
-                buy_ads.append(ad)
-            elif ad['buy_sell'] == 'SELL':
-                sell_ads.append(ad)
+                token = ad['token']
+                if token not in buy_ads: buy_ads[token] = []
+                buy_ads[token].append(ad)
 
-        buy_ads.sort(key=lambda x: x['price'], reverse=False)
-        sell_ads.sort(key=lambda x: x['price'], reverse=True)
+            elif ad['buy_sell'] == 'SELL':
+                token = ad['token']
+                if token not in sell_ads: sell_ads[token] = []
+                sell_ads[token].append(ad)
+
+        for ads in buy_ads.values():
+            ads.sort(key=lambda x: x['price'], reverse=False)
+
+        for ads in sell_ads.values():
+            ads.sort(key=lambda x: x['price'], reverse=True)
 
         return buy_ads, sell_ads
-
-
-    def create_answer(self, trade_type: str, total: int) -> str:
-        return f'Count in 2 actions: {trade_type} --> {total}\n'
 
 
     def create_response(self, result: list) -> dict:
@@ -214,18 +220,23 @@ class BaseCount(BaseP2P):
         return response
 
 
-    def get_ads(self) -> dict:
-        query, params = self.create_exchange_query()
-        result = self.execute_exchange_query(query, params)
-        print(f'Result -- {len(result)}')
+    def get_ads(self) -> tuple:
+        result = self.execute_exchange_query()
         ads = self.create_response(result)
-        buy_list, sell_list = self.split_and_sort_ads(ads)
-        data = {'buy': buy_list,
-                'sell': sell_list} 
-        return data
+        buy_dict, sell_dict = self.split_and_sort_ads(ads)
+        return buy_dict, sell_dict
 
 
 class CountActionsInTwo(BaseCount):
+    def __init__(self):
+        super().__init__()
+        self.total_links = {}
+
+
+    def create_answer(self, trade_type: str, total: int) -> str:
+        return f'Count in 2 actions: {trade_type} --> {total}\n'
+
+
     def count_spread(self, buy_price: float, sell_price: float) -> float:
         spread = ((sell_price / buy_price) - 1) * 100
         spread = round(spread, 2)
@@ -236,109 +247,113 @@ class CountActionsInTwo(BaseCount):
         return f'{trade_type}--{token}--{2}'
     
 
-    def count(self, total_links: dict, buy_ads: dict,
+    def count(self, buy_ads: dict,
               sell_ads: dict, trade_type: str) -> str:
 
-        for buy_ad in buy_ads:
-            buy_token = buy_ad['token']
-            buy_price = buy_ad['price']
+        for token in self.tokens:
+            key = self.create_key(trade_type, token)
+            self.total_links[key] = []
+            
+            buy_ads_by_token = buy_ads.get(token)
+            sell_ads_by_token = sell_ads.get(token)
+            if not buy_ads_by_token or not sell_ads_by_token: continue
 
-            for sell_ad in sell_ads:
-                sell_token = sell_ad['token']
-                sell_price = sell_ad['price']
+            for buy_ad in buy_ads_by_token:
+                buy_price = buy_ad['price']
 
-                if buy_token != sell_token: continue
+                for sell_ad in sell_ads_by_token:
+                    sell_price = sell_ad['price']
 
-                key = self.create_key(trade_type, buy_token)
-                if key not in total_links:
-                    total_links[key] = []
-                
-                spread = self.count_spread(buy_price, sell_price)
+                    spread = self.count_spread(buy_price, sell_price)
 
-                if spread < self.min_spread: continue
-                total_links[key].append({
-                    'spread': spread,
-                    '1': buy_ad,
-                    '2': sell_ad,
-                })
+                    if spread < self.min_spread: continue
+                    self.total_links[key].append({'spread': spread,
+                                                '1': buy_ad,
+                                                '2': sell_ad})
 
 
-    def save(self, total_links: dict) -> str:
+    def save(self) -> int:
         BestLinksInTwoActions.objects.all().delete()
 
-        for key, values in total_links.items():
+        n = 0
+        for key, values in self.total_links.items():
             if len(values) == 0: continue
+            n += len(values)
             cache.set(key, values, self.time_cash)
+        return n
             
-            for ad in values:
-                buy_ad_data = ad['1']
-                sell_ad_data = ad['2']
-                spread = ad['spread']
+            # for ad in values:
+            #     buy_ad_data = ad['1']
+            #     sell_ad_data = ad['2']
+            #     spread = ad['spread']
 
-                buy_ad = AdsInTwo.objects.create(**buy_ad_data)
-                sell_ad = AdsInTwo.objects.create(**sell_ad_data)
-                spread = BestLinksInTwoActions.objects.create(
-                    buy_ad=buy_ad, sell_ad=sell_ad, spread=spread
-                )
+            #     buy_ad = AdsInTwo.objects.create(**buy_ad_data)
+            #     sell_ad = AdsInTwo.objects.create(**sell_ad_data)
+            #     spread = BestLinksInTwoActions.objects.create(
+            #         buy_ad=buy_ad, sell_ad=sell_ad, spread=spread
+            #     )
 
 
     def count_in_two_actions(self) -> None:
-        dict = self.get_ads()
+        buy_dict, sell_dict = self.get_ads()
 
-        total_links = {} 
-
-        self.count(total_links, dict['buy'], dict['buy'], self.trade_types['b-b'])
-        self.count(total_links, dict['buy'], dict['sell'], self.trade_types['b-s'])
-        self.count(total_links, dict['sell'], dict['buy'], self.trade_types['s-b'])
-        self.count(total_links, dict['sell'], dict['sell'], self.trade_types['s-s'])
+        self.count(buy_dict, buy_dict, self.trade_types['b-b'])
+        self.count(buy_dict, sell_dict, self.trade_types['b-s'])
+        self.count(sell_dict, buy_dict, self.trade_types['s-b'])
+        self.count(sell_dict, sell_dict, self.trade_types['s-s'])
         
-        self.save(total_links)
-        return
+        number_of_links = self.save()
+        return number_of_links
     
 
 class CountActionsInThree(BaseCount):
+    def create_key(self, trade_type: str, token: str) -> str:
+        return f'{trade_type}--{token}--{3}'
+    
+
     def get_key(self, fiat: str) -> str:
-        if fiat == 'BTC':
-            return 'BTCUSDT'
-        elif fiat == 'GMT':
-            return 'GMTUSDT'
-        elif fiat == 'XMR':
-            return 'XMRUSDT'
-        elif fiat == 'DOGE':
-            return 'DOGEUSDT'
-        elif fiat == 'TRX':
-            return 'TRXUSDT'
-        elif fiat == 'EOS':
-            return 'EOSUSDT'
-        elif fiat == 'XRP':
-            return 'XRPUSDT'
-        elif fiat == 'LTC':
-            return 'LTCUSDT'
-        elif fiat == 'BUSD':
-            return 'BUSDUSDT'
-        elif fiat == 'BNB':
-            return 'BNBUSDT'
-        elif fiat == 'ETH':
-            return 'ETHUSDT'
-        elif fiat == 'RUB':
-            return 'USDTRUB'
-        elif fiat == 'SHIB':
-            return 'SHIBUSDT'
-        elif fiat == 'USDC':  
-            return 'USDCUSDT'
-        elif fiat == 'DAI':  
-            return 'DAIUSDT' 
-        elif fiat == 'TON':
-            return 'TON_USDT'
-        elif fiat == 'HT':
-            return 'HT_USDT'
-        else:
-            return fiat
+        match fiat:
+            case 'BTC':
+                return 'BTCUSDT'
+            case 'GMT':
+                return 'GMTUSDT'
+            case 'XMR':
+                return 'XMRUSDT'
+            case 'DOGE':
+                return 'DOGEUSDT'
+            case 'TRX':
+                return 'TRXUSDT'
+            case 'EOS':
+                return 'EOSUSDT'
+            case 'XRP':
+                return 'XRPUSDT'
+            case 'LTC':
+                return 'LTCUSDT'
+            case 'BUSD':
+                return 'BUSDUSDT'
+            case 'BNB':
+                return 'BNBUSDT'
+            case 'ETH':
+                return 'ETHUSDT'
+            case 'RUB':
+                return 'USDTRUB'
+            case 'SHIB':
+                return 'SHIBUSDT'
+            case 'USDC':
+                return 'USDCUSDT'
+            case 'DAI':
+                return 'DAIUSDT'
+            case 'TON':
+                return 'TON_USDT'
+            case 'HT':
+                return 'HT_USDT'
+            case _:
+                return None
 
 
     def get_price_by_symbol(self, crypto: dict, target_crypto: str) -> float:
         price = crypto.get(target_crypto, None)
-        if price: 
+        if price is not None: 
             return float(price)
         return price
 
@@ -354,49 +369,6 @@ class CountActionsInThree(BaseCount):
         return spread
 
 
-    def count(self, buy_ads: list, sell_ads: list, spot) -> dict:
-        links = {}
-
-        for buy_ad in buy_ads:
-            buy_token = buy_ad['token']
-
-            for sell_ad in sell_ads:
-                sell_token = sell_ad['token']
-
-                if buy_token == 'USDT' and sell_token != 'USDT':
-                    reverse = False
-                    token = sell_token
-                elif buy_token != 'USDT' and sell_token == 'USDT':
-                    reverse = True
-                    token = buy_token
-                else:
-                    continue
-                
-                key = self.get_key(token)
-                spot_price = self.get_price_by_symbol(spot, key)
-
-                if spot_price == None:  continue
-
-                spread = self.count_spread(buy_ad['price'], 
-                                            sell_ad['price'], 
-                                            spot_price,
-                                            reverse=reverse)
-                
-                if spread < self.min_spread: continue
-
-                if token not in links:
-                    links[token] = []
-
-                record = {'spread': spread,
-                            'spot': spot_price,
-                            '1': buy_ad,
-                            '2': sell_ad}
-                
-                links[token].append(record)
-
-        return links
-
-
     def binance_spot(self) -> dict:
         url = "https://api.binance.com/api/v3/ticker/price"
         response = requests.get(url)
@@ -408,47 +380,85 @@ class CountActionsInThree(BaseCount):
             price = token_info['price']
             spot_prices[symbol] = price
         return spot_prices
-
-
-    def create_key(self, trade_type: str, token: str) -> str:
-        return f'{trade_type}--{token}--{3}'
     
 
-    def save(self, total_links: dict) -> str:
+    def count(self, buy_ads: list, sell_ads: list, spot) -> dict:
+        links = {}
+        for token in self.tokens:
+            links[token] = []
+
+        for buy_token, buy_ads_bu_token in buy_ads.items():
+            for buy_ad in buy_ads_bu_token:
+            
+                for sell_token, sell_ads_bu_token in sell_ads.items():
+                    for sell_ad in sell_ads_bu_token:
+
+                        if buy_token == 'USDT' and sell_token != 'USDT':
+                            reverse = False
+                            token = sell_token
+                        elif buy_token != 'USDT' and sell_token == 'USDT':
+                            reverse = True
+                            token = buy_token
+                        else:
+                            continue
+                        
+                        key = self.get_key(token)
+                        spot_price = self.get_price_by_symbol(spot, key)
+
+                        if spot_price == None:  continue
+
+                        spread = self.count_spread(buy_ad['price'], 
+                                                    sell_ad['price'], 
+                                                    spot_price,
+                                                    reverse=reverse)
+                        
+                        if spread < self.min_spread: continue
+                        
+                        links[token].append({'spread': spread,
+                                            'spot': spot_price,
+                                            '1': buy_ad,
+                                            '2': sell_ad})
+
+        return links
+    
+
+    def save(self, total_links: dict) -> int:
         BestLinksInThreeActions.objects.all().delete()
 
+        n = 0
         for trade_type, ads in total_links.items():
             for token, links in ads.items():
                 if len(links) == 0: continue
+                n += len(links)
                 key = self.create_key(trade_type, token)
                 cache.set(key, links, self.time_cash)
+        return n 
+                # for ad in links:
+                #     buy_ad_data = ad['1']
+                #     sell_ad_data = ad['2']
+                #     spread = ad['spread']
+                #     spot = ad['spot']
 
-                for ad in links:
-                    buy_ad_data = ad['1']
-                    sell_ad_data = ad['2']
-                    spread = ad['spread']
-                    spot = ad['spot']
-
-                    buy_ad = AdsInThree.objects.create(**buy_ad_data)
-                    sell_ad = AdsInThree.objects.create(**sell_ad_data)
-                    spread = BestLinksInThreeActions.objects.create(
-                        buy_ad=buy_ad, sell_ad=sell_ad, spread=spread, spot=spot
-                    )
+                #     buy_ad = AdsInThree.objects.create(**buy_ad_data)
+                #     sell_ad = AdsInThree.objects.create(**sell_ad_data)
+                #     spread = BestLinksInThreeActions.objects.create(
+                #         buy_ad=buy_ad, sell_ad=sell_ad, spread=spread, spot=spot
+                #     )
 
 
     def count_in_three_actions(self) -> None:
-        dict = self.get_ads()
+        buy_dict, sell_dict = self.get_ads()
         spot = self.binance_spot()
 
         data = {
-            self.trade_types['b-b']: self.count(dict['buy'], dict['buy'], spot),
-            self.trade_types['b-s']: self.count(dict['buy'], dict['sell'], spot),
-            self.trade_types['s-b']: self.count(dict['sell'], dict['buy'], spot),
-            self.trade_types['s-s']: self.count(dict['sell'], dict['sell'], spot)
+            self.trade_types['b-b']: self.count(buy_dict, buy_dict, spot),
+            self.trade_types['b-s']: self.count(buy_dict, sell_dict, spot),
+            self.trade_types['s-b']: self.count(sell_dict, buy_dict, spot),
+            self.trade_types['s-s']: self.count(sell_dict, sell_dict, spot)
         }
 
-        self.save(data)
-        return
+        number_of_links = self.save(data)
+        return number_of_links
         
 
         
